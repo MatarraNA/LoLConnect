c0d2f9a5b850223b1804f04c1108f93697d00a8a
vincent ayala <vinny545@yahoo.com>
2019-06-17 04:32:19 -0400

    Add project files.

---
+++ LoLConnect/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace LoLConnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create In-House Lobby";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(203, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Lobby Code";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(221, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 22);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Copy";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(12, 303);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(203, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Lobby Code";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(12, 257);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(280, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Join In-House Lobby";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(221, 303);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(71, 22);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Paste";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(61, 343);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(185, 22);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Made by Matarra v1.0.0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 86);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(280, 51);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Enter += new System.EventHandler(this.richTextBox1_Enter);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(12, 143);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(280, 28);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Invite Players";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(304, 377);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "LoL In-House Connect";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ LoLConnect/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LCUSharp;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Remoting.Contexts;
+using System.Reflection;
+
+namespace LoLConnect
+{
+    public partial class Form1 : Form
+    {
+        // create api
+        LeagueClientApi api = null;
+        string lobbyPassword = "hard password";
+        string summonerName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            api = await LCUSharp.LeagueClientApi.ConnectAsync();
+            await api.RiotClientEndpoint.ShowUxAsync();
+
+            // create game
+            await CreateCustomGame();
+
+            // after game creation, obtain lobby id
+            await Task.Delay(1000);
+            var lobby = await GetCustomLobby();
+            textBox1.Text = lobby.id.ToString();
+        }
+
+        public async Task<CustomGameListRoot> GetCustomLobby()
+        {
+            // join custom game by id
+            string url = $"lol-lobby/v1/custom-games";
+            var queryParameters = Enumerable.Empty<string>();
+            try
+            {
+                // post first
+                await api.RequestHandler.GetJsonResponseAsync(HttpMethod.Post, "lol-lobby/v1/custom-games/refresh",
+                                                                         queryParameters);
+                await Task.Delay(100);
+
+                var json = await api.RequestHandler.GetJsonResponseAsync(HttpMethod.Get, url,
+                                                                         queryParameters);
+                var output = JsonConvert.DeserializeObject<List<CustomGameListRoot>>(json);
+                foreach( var lobby in output )
+                {
+                    if( lobby.ownerSummonerName.Equals( await GetSummonerName(), StringComparison.CurrentCultureIgnoreCase ) || lobby.lobbyName.Contains(await GetSummonerName()))
+                    {
+                        // this is the LOBBY
+                        return lobby;
+                    }
+                }
+            }
+            catch ( Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return null;
+        }
+
+        public async Task<string> GetSummonerName()
+        {
+            if (summonerName != "") return summonerName;
+            var queryParameters = Enumerable.Empty<string>();
+            try
+            {
+                var json = await api.RequestHandler.GetJsonResponseAsync(HttpMethod.Get, "lol-summoner/v1/current-summoner",
+                                                                         queryParameters);
+                var output = JsonConvert.DeserializeObject<SummonerRoot>(json);
+                summonerName = output.displayName;
+                return output.displayName;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return "null";
+            }
+        }
+
+        public async Task CreateCustomGame()
+        {
+            // Update the current summoner's profile icon to 23.
+            CreateCustomGameRoot obj = new CreateCustomGameRoot();
+            obj.isCustom = true;
+            CustomGameLobby lobby = new CustomGameLobby();
+            obj.customGameLobby = lobby;
+            // random seed
+            int random = new Random().Next(100);
+            lobby.lobbyName = (await GetSummonerName()) + "'s In-House Lobby " + random;
+            lobby.lobbyPassword = lobbyPassword;
+            Configuration config = new Configuration();
+            lobby.configuration = config;
+            config.gameMode = "CLASSIC";
+            config.gameMutator = "GAME_CFG_DRAFT_TOURNAMENT";
+            config.gameServerRegion = "";
+            config.spectatorPolicy = "AllAllowed";
+            config.teamSize = 5;
+            config.mapId = 11;
+            config.mutators = new Mutators();
+            config.mutators.id = 6;
+            var queryParameters = Enumerable.Empty<string>();
+            try
+            {
+                var json = await api.RequestHandler.GetJsonResponseAsync(HttpMethod.Post, "lol-lobby/v2/lobby",
+                                                                         queryParameters, obj);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException);
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public class Mutators
+        {
+            public int id { get; set; }
+        }
+
+        public class Configuration
+        {
+            public string gameMode { get; set; }
+            public string gameMutator { get; set; }
+            public string gameServerRegion { get; set; }
+            public int mapId { get; set; }
+            public Mutators mutators { get; set; }
+            public string spectatorPolicy { get; set; }
+            public int teamSize { get; set; }
+        }
+
+        public class CustomGameLobby
+        {
+            public Configuration configuration { get; set; }
+            public string lobbyName { get; set; }
+            public object lobbyPassword { get; set; }
+        }
+
+        public class CreateCustomGameRoot
+        {
+            public CustomGameLobby customGameLobby { get; set; }
+            public bool isCustom { get; set; }
+        }
+
+        public class RerollPoints
+        {
+            public int currentPoints { get; set; }
+            public int maxRolls { get; set; }
+            public int numberOfRolls { get; set; }
+            public int pointsCostToRoll { get; set; }
+            public int pointsToReroll { get; set; }
+        }
+
+        public class SummonerRoot
+        {
+            public long accountId { get; set; }
+            public string displayName { get; set; }
+            public string internalName { get; set; }
+            public int percentCompleteForNextLevel { get; set; }
+            public int profileIconId { get; set; }
+            public string puuid { get; set; }
+            public RerollPoints rerollPoints { get; set; }
+            public long summonerId { get; set; }
+            public int summonerLevel { get; set; }
+            public int xpSinceLastLevel { get; set; }
+            public int xpUntilNextLevel { get; set; }
+        }
+
+        public class CustomGameListRoot
+        {
+            public int filledPlayerSlots { get; set; }
+            public int filledSpectatorSlots { get; set; }
+            public string gameType { get; set; }
+            public bool hasPassword { get; set; }
+            public object id { get; set; }
+            public string lobbyName { get; set; }
+            public int mapId { get; set; }
+            public int maxPlayerSlots { get; set; }
+            public int maxSpectatorSlots { get; set; }
+            public string ownerSummonerName { get; set; }
+            public string passbackUrl { get; set; }
+            public string spectatorPolicy { get; set; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Clipboard.GetText();
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            api = await LCUSharp.LeagueClientApi.ConnectAsync();
+            await api.RiotClientEndpoint.ShowUxAsync();
+
+            // join it!
+            await JoinCustomGame();
+
+        }
+
+        public async Task JoinCustomGame()
+        {
+            // join custom game by id
+            api = await LCUSharp.LeagueClientApi.ConnectAsync();
+            string url = $"lol-lobby/v1/custom-games/{textBox2.Text}/join";
+            var body = new { asSpectator = false, password = lobbyPassword };
+            var queryParameters = Enumerable.Empty<string>();
+            try
+            {
+                var json = await api.RequestHandler.GetJsonResponseAsync(HttpMethod.Post, url,
+                                                                         queryParameters, body);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void richTextBox1_Enter(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            api = await LCUSharp.LeagueClientApi.ConnectAsync();
+            await api.RiotClientEndpoint.ShowUxAsync();
+
+            // parse all players names from textbox
+            string unparsed = richTextBox1.Text;
+            string[] namesArr = unparsed.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            for(int i = 0; i < namesArr.Length; i++ )
+            {
+                // remove white space and everything before including the period
+                namesArr[i] = namesArr[i].Replace(" ", "");
+                if(namesArr[i].Contains(".") )
+                {
+                    // remove everything before and upto dot
+                    namesArr[i] = namesArr[i].Substring(namesArr[i].IndexOf('.') + 1);
+                }
+                // ready to go!
+            }
+            foreach( var playerName in namesArr )
+            {
+                await InvitePlayer(playerName);
+            }
+        }
+
+        public async Task<SummonerRoot> GetSummonerAsync(string name )
+        {
+            string url = $"lol-summoner/v1/summoners";
+            IEnumerable<string> queryParameters = new string[] { $"name={name}" };
+            try
+            {
+                var json = await api.RequestHandler.GetJsonResponseAsync(HttpMethod.Get, url,
+                                                                         queryParameters);
+
+                var summoner = JsonConvert.DeserializeObject<SummonerRoot>(json);
+
+
+                // can now invite summoner
+                return summoner;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        public async Task InvitePlayer( string name )
+        {
+            // join custom game by id
+            var summoner = await GetSummonerAsync(name);
+
+            //inv that player!
+            string url = $"lol-lobby/v2/lobby/invitations";
+            IEnumerable<string> queryParameters = Enumerable.Empty<string>();
+            var player = new List<PlayerToInvite>();
+            player.Add(new PlayerToInvite());
+            player.FirstOrDefault().toSummonerId = summoner.summonerId.ToString();
+            player.FirstOrDefault().toSummonerName = summoner.displayName;
+
+            try
+            {
+                var json = await api.RequestHandler.GetJsonResponseAsync(HttpMethod.Post, url, queryParameters, player);
+            }
+            catch( Exception ex )
+            {
+                MessageBox.Show($"Failed to invite {summoner.displayName}\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public class PlayerToInvite
+        {
+            public string toSummonerId;
+            public string toSummonerName;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
